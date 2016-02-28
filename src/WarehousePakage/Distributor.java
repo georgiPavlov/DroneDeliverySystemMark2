568a1942dbc9f7c4f0188c29ff7cc0af76d6433c
georgiPavlov <georgi95.bg@gmail.com>
2016-02-29 00:30:34 +0200

    update

--- src/WarehousePakage/Distributor.java
+++ src/WarehousePakage/Distributor.java
@@ -1,6 +1,9 @@
 package WarehousePakage;
 
+import Calculations.CalculateDate;
+import Calculations.CalculateParameters;
 import DronePakage.Drone;
+import DronePakage.DroneDeliveryWindows;
 
 import java.util.List;
 
@@ -46,7 +49,52 @@ public class Distributor extends Thread {
     }
 
     private void startOperation(Order order) {
+           int requaredDrones = CalculateParameters.getNumberOfRequiredDrones(order);
+        for (int i = 0; i < requaredDrones ; i++) {
+            addToDroneOrder(order);
 
+        }
 
     }
+
+    private void addToDroneOrder(Order order) {
+        for (int i = 0; i < drones.size() ; i++) {
+            currentDroneDeliveryWindows(order,drones.get(i));
+        }
+    }
+
+    private void currentDroneDeliveryWindows(Order order, Drone drone) {
+        List<DroneDeliveryWindows> windows = drone.getDroneDeliveryWindowsList();
+        for (int i = 0; i < windows.size() ; i++) {
+            if(tryWindow(order.getDate(),windows.get(i))){
+
+            }
+        }
+    }
+
+    private boolean tryWindow(String date, DroneDeliveryWindows droneDeliveryWindows) {
+        long [] distanceWindow = CalculateDate.getChronoUnits(droneDeliveryWindows.getStartTime(),
+                droneDeliveryWindows.getEndTime());
+        long [] distanceDate = CalculateDate.getChronoUnits(droneDeliveryWindows.getStartTime(),
+                date);
+        if(distanceDate[0] == 0){
+            return  false;
+        }
+
+        if(tryDistance(distanceDate,distanceWindow)){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean tryDistance(long[] distanceDate, long[] distanceWindow) {
+        for (int i = 0; i < distanceDate.length; i++) {
+            if(distanceDate[i] > distanceWindow[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
