dc00ce1a4b54da5a51b18ea949fe0fe7a68e4d2a
georgiPavlov <georgi95.bg@gmail.com>
2016-02-29 21:35:22 +0200

    update

---
+++ src/DroneDataBacePakage/makeOperations.java
@@ -0,0 +1,14 @@
+package DroneDataBacePakage;
+
+import WarehousePakage.Warehouse;
+
+/**
+ * Created by georgipavlov on 29.02.16.
+ */
+public class makeOperations {
+     private Warehouse warehouse;
+
+    public makeOperations(Warehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+}
